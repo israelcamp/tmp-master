5c2af71045d0d068633fecaf058168f957f3101a
Israel Campiotti <israelcampiotti@gmail.com>
2023-05-29 13:55:07 -0300

    imgaug augs

--- trainer/datamodule/iam.py
+++ trainer/datamodule/iam.py
@@ -9,26 +9,26 @@ from .augs import AlbumentationsBackend
 
 @dataclass
 class IAMDataModule(SROIETask2DataModule):
-    def train_augs(
-        self,
-    ):
-        rotate = A.Affine(
-            rotate=(-1, 1),
-            translate_px=(0, 10),
-            fit_output=True,
-            keep_ratio=True,
-            cval=(255, 255, 255),
-        )
-        gaussian = A.GaussianBlur(blur_limit=(3, 3), sigma_limit=(0.1, 2.0))
-        downscale = A.Downscale(scale_min=0.85, scale_max=0.95)
-        tfms = A.Compose(
-            [
-                rotate,
-                gaussian,
-                downscale,
-            ]
-        )
-        return AlbumentationsBackend(tfms=tfms)
+    # def train_augs(
+    #     self,
+    # ):
+    #     rotate = A.Affine(
+    #         rotate=(-1, 1),
+    #         translate_px=(0, 10),
+    #         fit_output=True,
+    #         keep_ratio=True,
+    #         cval=(255, 255, 255),
+    #     )
+    #     gaussian = A.GaussianBlur(blur_limit=(3, 3), sigma_limit=(0.1, 2.0))
+    #     downscale = A.Downscale(scale_min=0.85, scale_max=0.95)
+    #     tfms = A.Compose(
+    #         [
+    #             rotate,
+    #             gaussian,
+    #             downscale,
+    #         ]
+    #     )
+    #     return AlbumentationsBackend(tfms=tfms)
 
     def get_name2label(self, key: str):
         lst_data = self.img2label[key]
